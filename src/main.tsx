5cdec4a02e1ee5cc254aa5ae8aa98ab731f648d0
Camilo Canclini <camilocanclini@gmail.com>
2023-07-25 23:12:24 -0300

    testing build

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
