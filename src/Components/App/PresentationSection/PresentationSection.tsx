1d39614ab4b0d57674b0ce8edc12bfa2745d2da6
Camilo Canclini <camilocanclini@gmail.com>
2023-07-25 23:01:46 -0300

    Adding bootstrap and some configurations

---
+++ src/Components/App/PresentationSection/PresentationSection.tsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+export default function PresentationSection(){
+  return (
+    <button type="button" className="btn btn-primary">
+      Click me!
+    </button>
+  );
+}
